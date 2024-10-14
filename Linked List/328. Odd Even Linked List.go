4e9fa58af48b1f95f5e3cac3eb17c62f2cb414ab
tpid-magang-deni <magang.deni@technopartner.co.id>
2024-10-14 15:56:51 +0700

    Create 328. Odd Even Linked List.go

---
+++ Linked List/328. Odd Even Linked List.go
@@ -0,0 +1,26 @@
+package Linked_List
+
+func oddEvenList(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	odd := head
+	even := head.Next
+	evenHead := even
+
+	for even != nil && odd != nil {
+		// Move odd node
+		odd.Next = even.Next
+		odd = odd.Next
+
+		// Move even node
+		even.Next = odd.Next
+		even = even.Next
+	}
+	// Link last odd node to the head of even list
+	odd.Next = evenHead
+
+	return head
+}
