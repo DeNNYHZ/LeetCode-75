c89831cf46ee386b9cd94bafe8cb3059b5979134
deni setiawan <iamdenisetiawan@gmail.com>
2024-10-22 10:35:01 +0700

    Update 104. Maximum Depth of Binary Tree.go

--- Binary Tree - DFS/104. Maximum Depth of Binary Tree.go
+++ Binary Tree - DFS/104. Maximum Depth of Binary Tree.go
@@ -1,5 +1,16 @@
 package Binary_Tree___DFS
 
 func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
 
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	if left > right {
+		return left + 1
+	} else {
+		return right + 1
+	}
 }
