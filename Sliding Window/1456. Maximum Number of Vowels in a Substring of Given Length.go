deb73a67b99cb7b1b46ca49188ad624f632d0d85
tpid-magang-deni <magang.deni@technopartner.co.id>
2024-11-06 15:52:02 +0700

    Update 1456. Maximum Number of Vowels in a Substring of Given Length.go

--- Sliding Window/1456. Maximum Number of Vowels in a Substring of Given Length.go
+++ Sliding Window/1456. Maximum Number of Vowels in a Substring of Given Length.go
@@ -1,5 +1,26 @@
 package Sliding_Window
 
 func maxVowels(s string, k int) int {
+	vowels := map[byte]bool{'a': true, 'e': true, 'i': true, 'o': true, 'u': true}
+	maxCount, currentCount := 0, 0
 
+	for i := 0; i < k; i++ {
+		if vowels[s[i]] {
+			currentCount++
+		}
+	}
+	maxCount = currentCount
+
+	for i := k; i < len(s); i++ {
+		if vowels[s[i-k]] {
+			currentCount--
+		}
+		if vowels[s[i]] {
+			currentCount++
+		}
+		if currentCount > maxCount {
+			maxCount = currentCount
+		}
+	}
+	return maxCount
 }
