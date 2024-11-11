cb0a060465db4405ca0f0337e243e7ae45256a8b
tpid-magang-deni <magang.deni@technopartner.co.id>
2024-11-11 15:34:13 +0700

    Update 452. Minimum Number of Arrows to Burst Balloons.go

--- Intervals/452. Minimum Number of Arrows to Burst Balloons.go
+++ Intervals/452. Minimum Number of Arrows to Burst Balloons.go
@@ -1,5 +1,25 @@
 package Intervals
 
+import "sort"
+
 func findMinArrowShots(points [][]int) int {
-	
+	if len(points) == 0 {
+		return 0
+	}
+
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][1] < points[j][1]
+	})
+
+	count := 1
+	end := points[0][1]
+
+	for _, point := range points[1:] {
+		if point[0] > end {
+			count++
+			end = point[1]
+		}
+	}
+
+	return count
 }
