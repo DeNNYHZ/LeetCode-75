e183e226796ec7cef3b88a6997b5baa8ea61ea38
tpid-magang-deni <magang.deni@technopartner.co.id>
2024-11-13 16:46:57 +0700

    Create 236. Lowest Common Ancestor of a Binary Tree.go

---
+++ Binary Tree - DFS/236. Lowest Common Ancestor of a Binary Tree.go
@@ -0,0 +1,18 @@
+package Binary_Tree___DFS
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left == nil {
+		return right
+	}
+	if right == nil {
+		return left
+	}
+	return root
+}
