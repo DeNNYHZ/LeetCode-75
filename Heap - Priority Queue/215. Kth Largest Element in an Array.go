acbdcc9bec75c83b407b8cc4c89f2f58d831ac14
tpid-magang-deni <magang.deni@technopartner.co.id>
2024-11-13 16:47:13 +0700

    Create 215. Kth Largest Element in an Array.go

---
+++ Heap - Priority Queue/215. Kth Largest Element in an Array.go
@@ -0,0 +1,37 @@
+package Heap___Priority_Queue
+
+import (
+	"container/heap"
+)
+
+// IntHeap is a type that implements a min-heap
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] } // Min-heap condition
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// findKthLargest function using IntHeap
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, num := range nums {
+		heap.Push(h, num)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
+	}
+	return heap.Pop(h).(int)
+}
